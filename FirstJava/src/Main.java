e4111ece86f0a32c2137b53a3054056adebf91b3
Waqas <wmunir232@gmail.com>
2023-03-03 11:33:38 +0500

    Learned input and output in Java

---
+++ FirstJava/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        For outputting on screen
+        System.out.println("Enter a string ");
+
+//        creating an instance of the input class
+        Scanner input = new Scanner(System.in);
+
+//        Taking input string and printing it out on the screen
+        System.out.println(input.nextLine());
+    }
+}
